793fcc49a0b88c55d5c613f17e37139581d45b22
Mayank-1-0 <mayankrai368@gmail.com>
2024-10-01 00:29:31 +0530

    Create pow.java

---
+++ Pow.java/pow.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+class pow{
+    public double Power(double x, long n){
+        if(n==0) return 1;
+        if(n<0) return 1/Power(x,-n) ;
+        if(n%2==1) return x*Power(x*x,(n-1)/2);
+        else{
+            return Power(x*x,n/2);
+        }
+
+    }
+    public static void main(String[] args) {
+        System.out.println("Enter the no.and its power:");
+        pow  me = new pow();
+        Scanner sc= new Scanner(System.in);
+        double x =sc.nextDouble();
+        long n= sc.nextLong();
+        Double Res= me.Power(x,n);
+        System.out.println("the result is:" +Res);
+
+
+
+
+    }
+}
